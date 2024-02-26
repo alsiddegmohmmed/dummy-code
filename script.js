2deeeabf34c997e07ce2c2b8cc3dd23833f20f0c
alsiddegmohmmed <alsiddeg.omer19990@gmail.com>
2024-02-26 16:48:56 +0300

    update

--- script.js
+++ script.js
@@ -1,76 +1,25 @@
-[].forEach.call(document.getElementsByClassName('tags-input'), function (el) {
-    let hiddenInput = document.createElement('input'),
-        mainInput = document.createElement('input'),
-        tags = [];
-    
-    hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', el.getAttribute('data-name'));
-
-    mainInput.setAttribute('type', 'text');
-    mainInput.classList.add('main-input');
-    mainInput.addEventListener('input', function () {
-        let enteredTags = mainInput.value.split(',');
-        if (enteredTags.length > 1) {
-            enteredTags.forEach(function (t) {
-                let filteredTag = filterTag(t);
-                if (filteredTag.length > 0)
-                    addTag(filteredTag);
-            });
-            mainInput.value = '';
-        }
-    });
-
-    mainInput.addEventListener('keydown', function (e) {
-        let keyCode = e.which || e.keyCode;
-        if (keyCode === 13 && mainInput.value.length === 0 && tags.length > 0) {
-            removeTag(tags.length - 1);
-        }
-    });
-
-    el.appendChild(mainInput);
-    el.appendChild(hiddenInput);
-
-    addTag('hello!');
-
-    function addTag (text) {
-        let tag = {
-            text: text,
-            element: document.createElement('span'),
-        };
-
-        tag.element.classList.add('tag');
-        tag.element.textContent = tag.text;
-
-        let closeBtn = document.createElement('span');
-        closeBtn.classList.add('close');
-        closeBtn.addEventListener('click', function () {
-            removeTag(tags.indexOf(tag));
-        });
-        tag.element.appendChild(closeBtn);
-
-        tags.push(tag);
-
-        el.insertBefore(tag.element, mainInput);
-
-        refreshTags();
+function sumRange(n,total = 0) {
+    if ( n <= 0 ) {
+        return total
+    } else {
+        return sumRange( n -1 , total + n ); 
     }
+}
 
-    function removeTag (index) {
-        let tag = tags[index];
-        tags.splice(index, 1);
-        el.removeChild(tag.element);
-        refreshTags();
-    }
 
-    function refreshTags () {
-        let tagsList = [];
-        tags.forEach(function (t) {
-            tagsList.push(t.text);
-        });
-        hiddenInput.value = tagsList.join(',');
+function power( n , x ) {
+    if (x == 0) {
+        return 1; 
+    } else {
+        return n * power( n , x - 1 ); 
     }
+}
 
-    function filterTag (tag) {
-        return tag.replace(/[^\w -]/g, '').trim().replace(/\W+/g, '-');
+
+function fact(n,total = 1) {
+    if ( n <= 0 ) {
+        return total
+    } else {
+        return fact( n -1 , total * n ); 
     }
-});
+}
