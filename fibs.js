1871708f7a2652b6637d263a4fde1a2a6b01fd27
alsiddegmohmmed <alsiddeg.omer19990@gmail.com>
2024-02-27 13:00:14 +0300

    made fibs function

---
+++ fibs.js
@@ -0,0 +1,25 @@
+function fibs (n) {
+    const arr = []; 
+    for (let i = 0; i <= n; i++) {
+        if (i === 0) {
+             arr.push(i)
+        } 
+        else if (i === 1) {
+             arr.push(i);
+        } 
+        else {
+            
+            arr.push(i + (i - 1)); 
+            
+        }
+    }
+    console.log(arr); 
+}
+
+
+
+console.log(fibs (2)); 
+
+
+
+
